148ae0497f90c069e927add64e3c2b9f2d39b8e0
huseyinYolasigmazoglu <huseyinyolasigmazoglu@hotmail.com>
2020-12-07 22:53:07 +0300

    first

---
+++ ByteCoin/Model/CoinData.swift
@@ -0,0 +1,13 @@
+//
+//  CoinData.swift
+//  ByteCoin
+//
+//  Created by Huseyin Yolasigmazoglu on 11/09/Huseyin Yolasigmazoglu.
+//  Copyright © Huseyin Yolasigmazoglu The Huseyin Yolasigmazoglu. All rights reserved.
+//
+
+import Foundation
+
+struct CoinData: Decodable {
+    let rate: Double
+}
